eacb77fbd156b4f8e55e7c2aa9ca8df6a996fa15
Graeme-Dunlop <graemedunlop1983@gmail.com>
2022-03-15 15:44:24 +0000

    finished MVP

--- console.py
+++ console.py
@@ -11,8 +11,22 @@ artist_repository.delete_all()
 artist_1 = Artist('Beyonce')
 artist_repository.save(artist_1)
 
+artist_2 = Artist('Arlo Parks')
+artist_repository.save(artist_2)
+
 album_1 = Album('Lemonade', 'pop', artist_1)
 album_repository.save(album_1)
 
+album_2 = Album('Collapsed in Sunbeams', 'pop', artist_2)
+album_repository.save(album_2)
+
 artist_repository.select(artist_1.id)
 album_repository.select(album_1.id)
+
+artists = artist_repository.select_all()
+for artist in artists:
+    print(artist.__dict__)
+
+albums = album_repository.select_all()
+for album in albums:
+    print(album.__dict__)
