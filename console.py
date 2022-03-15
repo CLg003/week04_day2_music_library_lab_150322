6c5b8e7dba541fdc4c7ff117672c26855fcad187
Graeme-Dunlop <graemedunlop1983@gmail.com>
2022-03-15 15:08:30 +0000

    lemonade is in

--- console.py
+++ console.py
@@ -2,7 +2,11 @@ from models.artist import Artist
 from models.album import Album
 
 import repositories.artist_repository as artist_repository
+import repositories.album_repository as album_repository
+
 
 artist_1 = Artist('Beyonce')
-
 artist_repository.save(artist_1)
+
+album_1 = Album('Lemonade', 'pop', artist_1)
+album_repository.save(album_1)
